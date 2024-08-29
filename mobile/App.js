ce11aa645a07d9c1755098ae8216289cb2238cb0
Sandeep <bsandeep@corp.untd.com>
2024-08-29 17:19:39 +0530

    inint

--- mobile/App.js
+++ mobile/App.js
@@ -1,21 +1,5 @@
-import { StatusBar } from 'expo-status-bar'
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import Enchant from './src/Enchant'
 
 export default function App() {
-  return (
-    <SafeAreaView className="bg-[#141719]" style={{ flex: 1 }}>
-      <StatusBar style="light" />
-      <View style={styles.container}>
-        <Text className="text-gray-200">Start Enchant!</Text>
-      </View>
-    </SafeAreaView>
-  )
+  return <Enchant />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
---
+++ mobile/src/Enchant.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+
+export default function Enchant() {
+  return (
+    <SafeAreaView className="bg-[#141719]" style={{ flex: 1 }}>
+      <StatusBar style="light" />
+      <View style={styles.container}>
+        <Text className="text-gray-200">Start Enchant!</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
